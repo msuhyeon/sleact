439e21d73d633023e89948c7d676fe126c4823d2
msh <suhyeon@globaleur.com>
2022-04-18 18:24:19 +0900

    - 기본 셋팅 - bable, webpack 설정 - webpack dev sever 설정 - react-router 설정

---
+++ setting/ts/layouts/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import LogIn from "@pages/LogIn";
+import SignUp from "@pages/SignUp";
+
+const App = () => {
+  return ( 
+      <Switch>
+        <Redirect exact path="/" to="/login" />
+        <Route path="/login" component={LogIn}/>
+        <Route path="/signup" component={SignUp}/>
+      </Switch>
+  ); 
+    
+};
+
+export default App;
